f3e8e675f1b19d45419f31c020c3c04dcfa94c38
edjunma <edjunma@gmail.com>
2019-10-21 07:10:07 -0400

    setup counter feature with class components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
+import Counter from './Counter';
 
 function App() {
-	return (
-		<div>
-			<button>-</button>
-			<span>0</span>
-			<button>+</button>
-		</div>
-	)
+	console.log('Render App');
+	return <Counter initialCount={0} />;
 }
 
 export default App;
