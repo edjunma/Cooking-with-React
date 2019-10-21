a0e44d808da17c934114073f690b079465a52a22
edjunma <edjunma@gmail.com>
2019-10-21 07:45:47 -0400

    setting up context api to store state

--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ThemeContext } from './App';
 
 export default class Counter extends Component {
 	constructor(props) {
@@ -12,17 +13,25 @@ export default class Counter extends Component {
 	render() {
 		console.log('Render Counter');
 		return (
-			<div>
-				<button onClick={() => this.changeCount(-1)}>-</button>
-				<span>{this.state.count}</span>
-				<button onClick={() => this.changeCount(1)}>+</button>
-			</div>
+			<ThemeContext.Consumer>
+				{style => (
+					<div>
+						<button style={style} onClick={() => this.changeCount(-1)}>
+							-
+						</button>
+						<span>{this.state.count}</span>
+						<button style={style} onClick={() => this.changeCount(1)}>
+							+
+						</button>
+					</div>
+				)}
+			</ThemeContext.Consumer>
 		);
 	}
 
 	changeCount(amount) {
 		this.setState(prevState => {
-			return { count: this.state.count + amount };
+			return { count: prevState.count + amount };
 		});
 	}
 }
--- src/CounterHooks.js
+++ src/CounterHooks.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { ThemeContext } from './App';
 
 export default function CounterHooks({ initialCount }) {
-  console.log("Render Counter Hooks")
+	console.log('Render Counter Hooks');
 	const [count, setCount] = useState(initialCount);
+	const style = useContext(ThemeContext);
 	return (
 		<div>
-			<button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+			<button style={style} onClick={() => setCount(prevCount => prevCount - 1)}>
+				-
+			</button>
 			<span>{count}</span>
-			<button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+			<button style={style} onClick={() => setCount(prevCount => prevCount + 1)}>
+				+
+			</button>
 		</div>
 	);
 }
